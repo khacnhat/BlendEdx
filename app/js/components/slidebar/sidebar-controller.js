9e3cb2a4de1d85f39280c0439a17e7b6ba9a1346
Nhat Nguyen Khac <nhatnk1@fsoft.com.vn>
2015-04-22 18:41:26 +0700

    second commit

---
+++ app/js/components/slidebar/sidebar-controller.js
@@ -0,0 +1,83 @@
+/**
+ * Created by macmini on 4/22/15.
+ */
+
+(function() {
+  angular
+    .module('BlendEdxApp')
+    .directive('bldSidebar', bldSidebar);
+
+  function bldSidebar(){
+    return{
+      templateUrl: 'js/components/slidebar/bld-sidebar.html',
+      controller: controllerFn,
+      controllerAs: 'bldSidebarCtrl',
+      link: linkFn,
+      bindToController: true
+    }
+
+    function controllerFn($scope){
+      var vm = this;
+      $scope.isVisible = true;
+
+      $scope.buddies = [
+        {
+          id: "abc",
+          name: "Mary A.",
+          avatar: "images/woman-5.jpg",
+          status: "online",
+          slogan: "Feeling groovy"
+        },
+        {
+          id: "abc",
+          name: "Mary A.",
+          avatar: "images/woman-5.jpg",
+          status: "online",
+          slogan: "Feeling groovy"
+        },
+        {
+          id: "abc",
+          name: "Mary A.",
+          avatar: "images/woman-5.jpg",
+          status: "online",
+          slogan: "Feeling groovy"
+        },
+        {
+          id: "abc",
+          name: "Mary A.",
+          avatar: "images/woman-5.jpg",
+          status: "online",
+          slogan: "Feeling groovy"
+        }
+      ];
+
+
+      $scope.toggle = function() {
+        $scope.isVisible = !$scope.isVisible;
+      }
+    }
+
+    function linkFn($scope, $element, $attr){
+      $scope.$watch(
+        'isVisible',
+        function(newValue, oldValue){
+          if(newValue === oldValue){
+            return;
+          }
+          if(newValue){
+            $element
+              .stop(true, true)
+              .animate({
+                right: '0'
+              }, 500);
+          }else{
+            $element
+              .stop(true, true)
+              .animate({
+                right: '-' + ($element.width() - 60) + 'px'
+              }, 500);
+          }
+      });
+    }
+  }
+})();
---
+++ app/js/controllers/main-controller.js
@@ -0,0 +1,12 @@
+/**
+ * Created by macmini on 4/22/15.
+ */
+
+angular
+    .module('BlendEdxApp')
+    .controller('MainController', MainController);
+
+function MainController($scope, $http){
+    var vm = this;
+
+}
